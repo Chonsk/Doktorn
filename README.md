84bd903cd688ab91c0aed984f140b3af9a0211b1
hannesAxiell <han@axiell.com>
2015-11-23 15:16:12 +0100

    first
    validation including luhn testing in place.
    confirmation inputs remain

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This is a static project, serve it however you want. Then access it via your browser, instructions are on the index page.
 
 Please use the builtin AngularJS functions as much as possible.
+
+-----------------------------------------------------------------
+To start up a server just do:
+cmd: "npm install http-server -g" //if not already installed
+
+and then
+cmd: "http-server C:\location\to\app"
+
+after that, just go localhost:8080 in browser
--- app.js
+++ app.js
@@ -1,25 +1,65 @@
 angular.module('testapp', [
-	'ngRoute',
+  'ngRoute',
 ]).
-config(function($routeProvider){
-$routeProvider
-  .when('/', {
-  	templateUrl: 'views/home.html',
-  	controller: 'homeCtrl',
-  })
-  .when('/register', {
-    templateUrl: 'views/register.html',
-    controller: 'registerCtrl',
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
-}).
-controller('homeCtrl', function($log){
+config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'homeCtrl',
+    })
+    .when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'registerCtrl',
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+})
+
+.controller('homeCtrl', function($log) {
   $log.debug('Welcome to the testapp!');
-}).
-controller('registerCtrl', function($scope, $log){
-	$scope.submit = function(user){
-		$log.debug('submit: user = ', user);
-	};
+})
+
+.controller('registerCtrl', function($scope, $log) {
+  $scope.submit = function(user) {
+    $log.debug('submit: user = ', user);
+  };
+})
+
+.directive('luhnCheck', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, element, attributes, ngModel) {
+
+      function luhnCheck(value) {
+        ngModel.$setValidity('luhn-check', luhnChk(value));
+        return value;
+      }
+
+      ngModel.$parsers.push(luhnCheck);
+      ngModel.$formatters.push(luhnCheck);
+    }
+
+  };
 });
+
+var luhnChk = function(luhn) {
+	if (luhn && luhn.length) {
+	  var len = luhn.length,
+	    mul = 0,
+	    prodArr = [
+	      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+	      [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
+	    ],
+	    sum = 0;
+
+	  while (len--) {
+	    sum += prodArr[mul][parseInt(luhn.charAt(len), 10)];
+	    mul ^= 1;
+	  }
+
+	  return sum % 10 === 0 && sum > 0;
+	}
+	return false; // empty luhn, return false (happens at initiation)
+};
